403bb407d432f38832ee3da128dd14e4ee66ce82
libertyfromthinking <libertyfromthinking@gmail.com>
2018-10-29 20:20:33 +0900

    after 8:20

--- bookservice/src/main/webapp/WEB-INF/views/products.jsp
+++ bookservice/src/main/webapp/WEB-INF/views/products.jsp
@@ -35,24 +35,19 @@
 			class="cg classic">${category[3].name }</span> <span
 			class="cg theater">${category[4].name }</span>
 	</article>
+	
 	<div id="message">
 		바로 예매 가능한 전시, 공연, 행사가 <span id="ro">${count}개</span> 있습니다.
 	</div>
 
-
 	<div class="elements">
-		
 	</div>
+	
 	<span><div id="more">더보기</div></span>
+	
 	<footer>
 		<a id="top" href="#ad">↑TOP</a>
 	</footer>
-
-
-
-
-
-
 
 	<script id="template" type="text/template">
 	<div class="element">
@@ -63,6 +58,7 @@
 		<span class="contents">filecontent</span>
 	</div>
 	</script>
+	
 	<script src="<c:url value='/js/test.js'/>"></script>
 
 </body>
--- bookservice/src/main/webapp/js/test.js
+++ bookservice/src/main/webapp/js/test.js
@@ -1,7 +1,3 @@
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
     var category = document.getElementById("category");
     var view = document.getElementsByClassName("cg");
@@ -9,25 +5,35 @@ document.addEventListener("DOMContentLoaded", function () {
     var addNum = 0;
     var pageNum = 0;
     var parent = add.parentNode;
+    var res = null;
     
-    function doAll(){
-    	addNum = 4;
-        pageNum = 0;
-        parent.appendChild(add);
+    function over() {
+        this.style.borderBottom = "3px solid green";
+    }
+    function out() {
+        this.style.borderBottom = "0px";
+    }
+    
+    function drawBorder(){
+    	parent.appendChild(add);
         for (var i = 0; i < view.length; i++) {
             view[i].style.borderBottom = "0px";
             view[i].onmouseover = over;
             view[i].onmouseout = out;
         }
-        view[0].onmouseout = null;
-        view[0].style.borderBottom = "3px solid green"
+        view[pageNum].onmouseout = null;
+        view[pageNum].style.borderBottom = "3px solid green"
+    }
+
+    
+    function ajax(){
+    	
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.status == 200 && xhr.readyState == 4) {
 
                 var res = JSON.parse(xhr.responseText);
-                var newcontent = '';
-                var getIndex = function () {
+                function getIndex() {
                     for (var i = 0; i < res.length; i++) {
                         if (res[i].productId != null) {
                             index = i;
@@ -36,7 +42,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
 
                 }
-                var index = getIndex();
+
+                var newcontent = '';
+                                var index = getIndex();
                 for (var i = 0; i < addNum; i++) {
                     for (var j = index; j < res.length; j++) {
                         if (res[i].id == res[j].productId && res[j].type == 'ma') {
@@ -55,263 +63,51 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementsByClassName("elements")[0].innerHTML = newcontent;
             }
         }
-        xhr.open("GET", "./category?cn=0");
+        xhr.open("GET", "./category?cn="+pageNum);
         xhr.send();
 
 
+    } 
+
+    function doAll(){
+    	addNum = 4;
+        pageNum = 0;
+        drawBorder();
+        ajax();
+        
     }
-    var over = function () {
-        this.style.borderBottom = "3px solid green";
-    }
-    var out = function () {
-        this.style.borderBottom = "0px";
-    }
+    
 
     category.addEventListener("click", function (evt) {
         if (evt.target.className == "cg view") {
             addNum = 4;
             pageNum = 1;
-            parent.appendChild(add);
-            for (var i = 0; i < view.length; i++) {
-                view[i].style.borderBottom = "0px";
-                view[i].onmouseover = over;
-                view[i].onmouseout = out;
-            }
-            view[1].onmouseout = null;
-            view[1].style.borderBottom = "3px solid green"
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-                    
-                    var res = JSON.parse(xhr.responseText);
-                    var newcontent = '';
-                    var getIndex = function () {
-                        for (var i = 0; i < res.length; i++) {
-                            if (res[i].productId != null) {
-                                index = i;
-                                return index;
-                            }
-                        }
-
-                    }
-                    var index = getIndex();
-                    for (var i = 0; i < addNum; i++) {
-                        for (var j = index; j < res.length; j++) {
-                            if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                                var template = document.getElementById("template").innerHTML;
-                                var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                                template = template.replace('filesource', filesource)
-                                    .replace('filedescription', res[i].description)
-                                    .replace('filecontent', res[i].content);
-
-                                newcontent += template;
-
-                            }
-                        }
-                    }
-                    document.getElementById("ro").innerHTML = index + "개";
-                    document.getElementsByClassName("elements")[0].innerHTML = newcontent;
-                }
-            }
-            xhr.open("GET", "./category?cn=1");
-            xhr.send();
-
+            drawBorder();
+            ajax();
         } else if (evt.target.className == "cg musical") {
         	addNum = 4;
             pageNum = 2;
-            parent.appendChild(add);
-            for (var i = 0; i < view.length; i++) {
-                view[i].style.borderBottom = "0px";
-                view[i].onmouseover = over;
-                view[i].onmouseout = out;
-            }
-            view[2].onmouseout = null;
-            view[2].style.borderBottom = "3px solid green"
-
-
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-
-                    var res = JSON.parse(xhr.responseText);
-                    var newcontent = '';
-                    var getIndex = function () {
-                        for (var i = 0; i < res.length; i++) {
-                            if (res[i].productId != null) {
-                                index = i;
-                                return index;
-                            }
-                        }
-
-                    }
-                    var index = getIndex();
-                    for (var i = 0; i < addNum; i++) {
-                        for (var j = index; j < res.length; j++) {
-                            if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                                var template = document.getElementById("template").innerHTML;
-                                var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                                template = template.replace('filesource', filesource)
-                                    .replace('filedescription', res[i].description)
-                                    .replace('filecontent', res[i].content);
-
-                                newcontent += template;
-
-                            }
-                        }
-                    }
-                    document.getElementById("ro").innerHTML = index + "개";
-                    document.getElementsByClassName("elements")[0].innerHTML = newcontent;
-                }
-            }
-            xhr.open("GET", "./category?cn=2");
-            xhr.send();
-
+            drawBorder();
+            ajax();
         } else if (evt.target.className == "cg concert") {
         	addNum = 4;
             pageNum = 3;
-            parent.appendChild(add);
-            for (var i = 0; i < view.length; i++) {
-                view[i].style.borderBottom = "0px";
-                view[i].onmouseover = over;
-                view[i].onmouseout = out;
-
-            }
-            view[3].onmouseout = null;
-            view[3].style.borderBottom = "3px solid green"
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-
-                    var res = JSON.parse(xhr.responseText);
-                    var newcontent = '';
-                    var getIndex = function () {
-                        for (var i = 0; i < res.length; i++) {
-                            if (res[i].productId != null) {
-                                index = i;
-                                return index;
-                            }
-                        }
-
-                    }
-                    var index = getIndex();
-                    for (var i = 0; i < addNum; i++) {
-                        for (var j = index; j < res.length; j++) {
-                            if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                                var template = document.getElementById("template").innerHTML;
-                                var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                                template = template.replace('filesource', filesource)
-                                    .replace('filedescription', res[i].description)
-                                    .replace('filecontent', res[i].content);
-
-                                newcontent += template;
-
-                            }
-                        }
-                    }
-                    document.getElementById("ro").innerHTML = index + "개";
-                    document.getElementsByClassName("elements")[0].innerHTML = newcontent;
-                }
-            }
-            xhr.open("GET", "./category?cn=3");
-            xhr.send();
-
-        } else if (evt.target.className == "cg classic") {
+            drawBorder();
+            ajax();
+            } else if (evt.target.className == "cg classic") {
         	addNum = 4;
             pageNum = 4;
-            parent.appendChild(add);
-            for (var i = 0; i < view.length; i++) {
-                view[i].style.borderBottom = "0px";
-                view[i].onmouseover = over;
-                view[i].onmouseout = out;
-            }
-            view[4].onmouseout = null;
-            view[4].style.borderBottom = "3px solid green"
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-
-                    var res = JSON.parse(xhr.responseText);
-                    var newcontent = '';
-                    var getIndex = function () {
-                        for (var i = 0; i < res.length; i++) {
-                            if (res[i].productId != null) {
-                                index = i;
-                                return index;
-                            }
-                        }
-
-                    }
-                    var index = getIndex();
-                    for (var i = 0; i < addNum; i++) {
-                        for (var j = index; j < res.length; j++) {
-                            if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                                var template = document.getElementById("template").innerHTML;
-                                var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                                template = template.replace('filesource', filesource)
-                                    .replace('filedescription', res[i].description)
-                                    .replace('filecontent', res[i].content);
-
-                                newcontent += template;
-
-                            }
-                        }
-                    }
-                    document.getElementById("ro").innerHTML = index + "개";
-                    document.getElementsByClassName("elements")[0].innerHTML = newcontent;
-                }
-            }
-            xhr.open("GET", "./category?cn=4");
-            xhr.send();
-
-        } else if (evt.target.className == "cg theater") {
+            drawBorder();
+            ajax();
+            } else if (evt.target.className == "cg theater") {
         	addNum = 4;
             pageNum = 5;
-            parent.appendChild(add);
-            for (var i = 0; i < view.length; i++) {
-                view[i].style.borderBottom = "0px";
-                view[i].onmouseover = over;
-                view[i].onmouseout = out;
-            }
-            view[5].onmouseout = null;
-            view[5].style.borderBottom = "3px solid green"
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-
-                    var res = JSON.parse(xhr.responseText);
-                    var newcontent = '';
-                    var getIndex = function () {
-                        for (var i = 0; i < res.length; i++) {
-                            if (res[i].productId != null) {
-                                index = i;
-                                return index;
-                            }
-                        }
-
-                    }
-                    var index = getIndex();
-                    for (var i = 0; i < addNum; i++) {
-                        for (var j = index; j < res.length; j++) {
-                            if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                                var template = document.getElementById("template").innerHTML;
-                                var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                                template = template.replace('filesource', filesource)
-                                    .replace('filedescription', res[i].description)
-                                    .replace('filecontent', res[i].content);
-
-                                newcontent += template;
-
-                            }
-                        }
-                    }
-                    document.getElementById("ro").innerHTML = index + "개";
-                    document.getElementsByClassName("elements")[0].innerHTML = newcontent;
-                }
-            }
-            xhr.open("GET", "./category?cn=5");
-            xhr.send();
-
+            drawBorder();
+            ajax();
         } else if (evt.target.className == "cg all") {
+            addNum = 4;
+            pageNum = 0;
+            drawBorder();
         	doAll();
         	        }
     });
