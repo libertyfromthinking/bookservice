225bfea02564a0f0027820179651e0f34dc74e72
libertyfromthinking <libertyfromthinking@gmail.com>
2018-11-10 15:05:03 +0900

    zz

--- bookservice/src/main/webapp/WEB-INF/views/products.jsp
+++ bookservice/src/main/webapp/WEB-INF/views/products.jsp
@@ -35,20 +35,21 @@
 			class="cg classic">${category[3].name }</span> <span
 			class="cg theater">${category[4].name }</span>
 	</article>
-	
+
 	<div id="message">
 		바로 예매 가능한 전시, 공연, 행사가 <span id="ro">${count}개</span> 있습니다.
 	</div>
 
-	<div class="elements">
-	</div>
-	
+	<div class="elements"></div>
+
 	<span><div id="more">더보기</div></span>
-	
+
 	<footer>
 		<a id="top" href="#ad">↑TOP</a>
 	</footer>
-
+	<script src="https://code.jquery.com/jquery-3.3.0.js"
+		integrity="sha256-TFWSuDJt6kS+huV+vVlyV1jM3dwGdeNWqezhTxXB/X8="
+		crossorigin="anonymous"></script>
 	<script id="template" type="text/template">
 	<div class="element">
 		<img class="pic" src="<c:url value='filesource'/>">
@@ -58,7 +59,7 @@
 		<span class="contents">filecontent</span>
 	</div>
 	</script>
-	
+
 	<script src="<c:url value='/js/test.js'/>"></script>
 
 </body>
--- bookservice/src/main/webapp/js/test.js
+++ bookservice/src/main/webapp/js/test.js
@@ -1,136 +1,132 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var thumb = document.getElementsByClassName("thumbnail");
-	thumb[0].style.transform = "TranslateX(0%)";
-	var f = 0;
+$("window").ready(function () {
+    var $thumb = $(".thumbnail")
+    $thumb.css('transform', 'TranslateX(0%)');
+    var f = 0;
 
-    var category = document.getElementById("category");
-    var view = document.getElementsByClassName("cg");
-    var add = document.getElementById("more");
+    var $category = $("#category")
+    var $view = $(".cg")
+    var $add = $("#more");
     var addNum = 0;
     var pageNum = 0;
-    var parent = add.parentNode;
+    var $parent = $add.parent();
     var res = null;
-    
+
     function over() {
         this.style.borderBottom = "3px solid green";
     }
+
     function out() {
         this.style.borderBottom = "0px";
     }
-    
-    function drawBorder(){
-    	parent.appendChild(add);
-        for (var i = 0; i < view.length; i++) {
-            view[i].style.borderBottom = "0px";
-            view[i].onmouseover = over;
-            view[i].onmouseout = out;
-        }
-        view[pageNum].onmouseout = null;
-        view[pageNum].style.borderBottom = "3px solid green"
+
+    function drawBorder() {
+        $parent.append($add);
+        $view.css({
+            "borderBottom": "0px",
+            "onmouseover": over,
+            "onmouseout": out
+        });
+
+        $view.eq(pageNum).css('onmouseout', 'null');
+        $view.eq(pageNum).css('borderBottom', '3px solid green');
     }
 
-    
-    function ajax(){
-    	
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.status == 200 && xhr.readyState == 4) {
 
-                var res = JSON.parse(xhr.responseText);
-                function getIndex() {
-                    for (var i = 0; i < res.length; i++) {
-                        if (res[i].productId != null) {
-                            index = i;
-                            return index;
-                        }
-                    }
+    function ajax() {
+        
+        $.ajax({
+            url : "./category",
+            data : "cn="+pageNum,
+            type : "GET",
+            dataType : "json"
+        })
+        .done(function($res){
+            var newcontent = '';
+            var $index = $res.findIndex(i=>i.productId!=null);
+            var resLength = $res.length;
+            
+            for (var i = 0; i < addNum; i++) {
+                for (var j = $index; j < resLength; j++) {
+                    if ($res[i].id == $res[j].productId && $res[j].type == 'ma') {
+                        var $template = $("#template").html;
+                        var filesource = 'img/' + $res[j].productId + '_' + $res[j].type + '_' + $res[j].productImageId + '.png';
+                        $template = $template.replace('filesource', filesource)
+                            .replace('filedescription', $res[i].description)
+                            .replace('filecontent', $res[i].content);
 
-                }
+                        newcontent += $template;
 
-                var newcontent = '';
-                                var index = getIndex();
-                for (var i = 0; i < addNum; i++) {
-                    for (var j = index; j < res.length; j++) {
-                        if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                            var template = document.getElementById("template").innerHTML;
-                            var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                            template = template.replace('filesource', filesource)
-                                .replace('filedescription', res[i].description)
-                                .replace('filecontent', res[i].content);
-
-                            newcontent += template;
-
-                        }
                     }
                 }
-                document.getElementById("ro").innerHTML = index + "개";
-                document.getElementsByClassName("elements")[0].innerHTML = newcontent;
             }
-        }
-        xhr.open("GET", "./category?cn="+pageNum);
-        xhr.send();
+            $("#ro").html = $index + "개";
+            $(".elements").html = newcontent;
+        })
+        .fail(function(xhr, status, errorThrown){})
+        .always(function(xhr, status) {
+            console.log("요청이 완료되었습니다!");
+        });
+        
+    }
 
-
-    } 
-
-    function doAll(){
-    	addNum = 4;
+    function doAll() {
+        addNum = 4;
         pageNum = 0;
         drawBorder();
         ajax();
-        
+
     }
-    
+
     function ex() {
 
-        thumb[f].style.transition = "transform 1s linear";
-        thumb[f].style.transform = "translateX(-100%)";
+        $thumb[f].css("transition","transform 1s linear");
+        $thumb[f].css("transform","translateX(-100%)");
 
-        for(var j=0;j<thumb.length;j++){
-            if(j!=f&&j!=(f+1)&&j!=(f-1)){
-                thumb[j].style.transition = "";
-                thumb[j].style.transform = "translateX(100%)";
+        for (var j = 0; j < $thumb.length; j++) {
+            if (j != f && j != (f + 1) && j != (f - 1)) {
+                $thumb[j].css("transition","");
+                $thumb[j].css("transform","translateX(100%)");
             }
         }
 
-        if (f == thumb.length - 1) {
-            for (var j = 0; j < thumb.length - 1; j++) {
+        if (f == $thumb.length - 1) {
+            for (var j = 0; j < $thumb.length - 1; j++) {
                 f = -1;
             }
         }
 
-        thumb[f + 1].style.transition = "transform 1s linear";
-        thumb[f + 1].style.transform = "translateX(0%)";
-        
+        $thumb[f + 1].css("transition","transform 1s linear");
+        $thumb[f + 1].css("transform","translateX(0%)");
+
         f++;
 
     }
 
-    
 
-    category.addEventListener("click", function (evt) {
+
+    $category.on("click", function (evt) {
         if (evt.target.className == "cg view") {
             addNum = 4;
             pageNum = 1;
             drawBorder();
             ajax();
         } else if (evt.target.className == "cg musical") {
-        	addNum = 4;
+            addNum = 4;
             pageNum = 2;
             drawBorder();
             ajax();
         } else if (evt.target.className == "cg concert") {
-        	addNum = 4;
+            addNum = 4;
             pageNum = 3;
             drawBorder();
             ajax();
-            } else if (evt.target.className == "cg classic") {
-        	addNum = 4;
+        } else if (evt.target.className == "cg classic") {
+            addNum = 4;
             pageNum = 4;
             drawBorder();
             ajax();
-            } else if (evt.target.className == "cg theater") {
-        	addNum = 4;
+        } else if (evt.target.className == "cg theater") {
+            addNum = 4;
             pageNum = 5;
             drawBorder();
             ajax();
@@ -138,73 +134,70 @@ document.addEventListener("DOMContentLoaded", function () {
             addNum = 4;
             pageNum = 0;
             drawBorder();
-        	doAll();
-        	        }
+            doAll();
+        }
     });
 
-    add.addEventListener("click", function (evt) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.status == 200 && xhr.readyState == 4) {
-
-                var res = JSON.parse(xhr.responseText);
-                var newcontent = '';
-
-                var getIndex = function () {
-                    for (var i = 0; i < res.length; i++) {
-                        if (res[i].productId != null) {
-                            index = i;
-                            return index;
-                        }
-                    }
-
-                }
-                var index = getIndex();
-                if (addNum + 4 < index) {
-                    addNum = addNum + 4;
-                }
-                else {
-                    addNum = index;
-                    parent.removeChild(add);
-                }
-                for (var i = 0; i < addNum; i++) {
-                    for (var j = index; j < res.length; j++) {
-                        if (res[i].id == res[j].productId && res[j].type == 'ma') {
-                            var template = document.getElementById("template").innerHTML;
-                            var filesource = 'img/' + res[j].productId + '_' + res[j].type + '_' + res[j].productImageId + '.png';
-                            template = template.replace('filesource', filesource)
-                                .replace('filedescription', res[i].description)
-                                .replace('filecontent', res[i].content);
-
-                            newcontent += template;
-
-                        }
-                    }
-                }
-                document.getElementById("ro").innerHTML = index + "개";
-                document.getElementsByClassName("elements")[0].innerHTML = newcontent;
+    $add.on("click", function (evt) {
+        $.ajax({
+            url : "./category",
+            data : "cn="+pageNum,
+            type : "GET",
+            dataType : "json"
+        })
+        .done(function($res){
+            var newcontent = '';
+            var $index = $res.findIndex(i=>i.productId!=null);
+            var resLength = $res.length;
+            if (addNum + 4 < index) {
+                addNum = addNum + 4;
+            } else {
+                addNum = $index;
+                $parent.removeChild($add);
             }
-        }
-        if (pageNum == 1) {
-            xhr.open("GET", "./category?cn=1");
-            xhr.send();
-        }else if(pageNum == 2){
-        	xhr.open("GET", "./category?cn=2");
-        	xhr.send();
-        }else if(pageNum == 3) {
-            xhr.open("GET", "./category?cn=3");
-            xhr.send();
-        }else if(pageNum == 4) {
-            xhr.open("GET", "./category?cn=4");
-            xhr.send();
-        }else if(pageNum == 5) {
-            xhr.open("GET", "./category?cn=5");
-            xhr.send();
-        }else if(pageNum == 0){
-        	xhr.open("GET", "./category?cn=0");
-        	xhr.send();
-        }
+            
+            for (var i = 0; i < addNum; i++) {
+                for (var j = $index; j < resLength; j++) {
+                    if ($res[i].id == $res[j].productId && $res[j].type == 'ma') {
+                        var $template = $("#template").html;
+                        var filesource = 'img/' + $res[j].productId + '_' + $res[j].type + '_' + $res[j].productImageId + '.png';
+                        $template = $template.replace('filesource', filesource)
+                            .replace('filedescription', $res[i].description)
+                            .replace('filecontent', $res[i].content);
+        
+                        newcontent += $template;
+        
+                    }
+                }
+            }
+            $("#ro").html = $index + "개";
+            $(".elements").html = newcontent;
+        })
+        .fail(function(xhr, status, errorThrown){})
+        .always(function(xhr, status) {
+            console.log("요청이 완료되었습니다!");
+        });
+        // if (pageNum == 1) {
+        //     xhr.open("GET", "./category?cn=1");
+        //     xhr.send();
+        // } else if (pageNum == 2) {
+        //     xhr.open("GET", "./category?cn=2");
+        //     xhr.send();
+        // } else if (pageNum == 3) {
+        //     xhr.open("GET", "./category?cn=3");
+        //     xhr.send();
+        // } else if (pageNum == 4) {
+        //     xhr.open("GET", "./category?cn=4");
+        //     xhr.send();
+        // } else if (pageNum == 5) {
+        //     xhr.open("GET", "./category?cn=5");
+        //     xhr.send();
+        // } else if (pageNum == 0) {
+        //     xhr.open("GET", "./category?cn=0");
+        //     xhr.send();
+        // }
     });
+    
     setInterval(ex, 3000);
     doAll();
 });
